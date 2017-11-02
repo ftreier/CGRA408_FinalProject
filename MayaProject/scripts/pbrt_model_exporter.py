1b88940ac187b51832657fb65d37ccac8ffcd2d0
Jack Purvis <emperorpurvis@gmail.com>
2017-11-02 23:56:31 +1300

    Correct camera positioning and field of view, objects placed and with simple materials

--- MayaProject/scripts/pbrt_model_exporter.py
+++ MayaProject/scripts/pbrt_model_exporter.py
@@ -1,8 +1,8 @@
 import maya.cmds as cmds
 
 
-file_path = "/home/purvisjack/Cgra408/FinalProject/CGRA408_FinalProject/scenes/"
-# file_path = "D:/Documents/Victoria/2017/Cgra408/FinalProject/CGRA408_FinalProject/scenes/"
+# file_path = "/home/purvisjack/Cgra408/FinalProject/CGRA408_FinalProject/scenes/"
+file_path = "D:/Documents/Victoria/2017/Cgra408/FinalProject/CGRA408_FinalProject/scenes/"
 
 
 def export_selected():
